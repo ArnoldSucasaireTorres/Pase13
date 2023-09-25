1ff9ab9b72a8f26e3f6a4d9fb9fa23ebec756e7b
ArnoldSucasaireTorres <64154905+ArnoldSucasaireTorres@users.noreply.github.com>
2023-09-25 11:59:55 -0500

    Update Script_9 BTI014.sql

--- Script_9 BTI014.sql
+++ Script_9 BTI014.sql
@@ -3,7 +3,7 @@
 ---------------------------------------------------
 
 
---POSICION DE NEGOCIOS --
+--POSICION DE NEGOCIOS -- nuevo
 DELETE from  BTI014 Where BTINOM ='CMACMISTI' AND BTISRVNOM = 'CMACMoraSBS' AND BTIMTDNOM = 'ObtenerResumenMoraSBS'; -- 0
 Insert into BTI014 (BTINOM,BTISRVNOM,BTISRVVER,BTIMTDNOM,BTIMTDDSC,BTIMTDNSBT,BTIMTDPGMNOM,BTIMTDPGMMTD,BTIMTDSTATUS,BTIMTDFPATH,BTIMTDENBTRA,BTIMTDESPGGX) values ('CMACMISTI','CMACMoraSBS','1','ObtenerResumenMoraSBS','Obtiene el resumen mora SBS según el perfil.',' ','pjaqm06r','execute','Validado            ',' ','N','S');
 
