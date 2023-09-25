902a35d84f17ab448d7f867c74b6b3dc061d86c1
ArnoldSucasaireTorres <64154905+ArnoldSucasaireTorres@users.noreply.github.com>
2023-09-25 11:59:02 -0500

    nuevo

--- Script_6 BTI007.sql
+++ Script_6 BTI007.sql
@@ -1,7 +1,7 @@
 ---------------------------------------------------
 ----------- PERMISO DE USUARIOS ---------------
 ---------------------------------------------------
---POSICION DE NEGOCIOS--
+--POSICION DE NEGOCIOS--mas comentarios
 Delete from BTI007 Where BTICANNOM = 'MMISTI' and BTINOM = 'CMACMISTI' and BTISRVNOM = 'CMACPosicionNegocio'; -- 0
 
 INSERT INTO BANTPROD.BTI007 (BTICANNOM, BTINOM, BTISRVNOM, BTISRVVER, BTIMTDNOM, BTIEXTUSU, BTIUSUHAB)
